41aa5cc92007195d6e52536c2ae585a01f748f61
charlottelabeaumont-cmyk <charlottelabeaumont@gmail.com>
2025-12-04 11:43:07 +0100

    Couche WMS

--- app/main.js
+++ app/main.js
@@ -1,21 +1,19 @@
 // Import toujours en premier 
 import './style.css';
-import {Map, View} from 'ol';
+import {Map, View} from 'ol'; //Import de deux choses à la fois 
 import { ImageWMS } from 'ol/source';
 import TileLayer from 'ol/layer/Tile';
 import ImageLayer from 'ol/layer/Image';
 import OSM from 'ol/source/OSM';
 
-
 // Difference entre const/var/let : 
 //const = immuable dans le sens de l'objet qu'elle rpz (mais on peut changer les params)
 // var = varibale changeable 
 
-
-// Création 
+// Création Tile Layer Pour les sources de couche qui fournissent des images pré-rendues et découpées en tuiles, organisées en grilles par niveaux de zoom pour des résolutions spécifiques. 
 const couche_osm = new TileLayer({ source: new OSM() });
 
-// Création d'une image WMS 
+// Création d'une image WMS : serveurs WMS fournissant des images uniques et non tuilées.
 const ma_source = new ImageWMS({
   url: 'https://ahocevar.com/geoserver/wms',
   params: { 'LAYERS' : 'topp:states' },
